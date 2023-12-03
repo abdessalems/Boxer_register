7d99d3a4877c09c63afbcf377acacbb408275e4a
Baganda49 <abdessalemsaa@gmail.com>
2023-12-03 12:36:22 +0100

    fix bugs

--- boxer__register/src/views/Login.vue
+++ boxer__register/src/views/Login.vue
@@ -1,13 +1,12 @@
 <!-- Login.vue -->
 <template>
   <div>
-    <h1>Login Page</h1>
+    <h1></h1>
     <LoginForm />
   </div>
 </template>
 
 <script>
-// eslint-disable-next-line
 import LoginForm from "@/components/Login/Login.vue";
 export default {
   name: "LoginView",
